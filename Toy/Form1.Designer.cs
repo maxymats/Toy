996e6d57a9e15c5076e3133e9995a997ce78ba36
maxymats <99792368+maxymats@users.noreply.github.com>
2023-05-30 19:24:19 +0300

    Toy

---
+++ Toy/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿
+namespace Toy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.penaltyLabel = new System.Windows.Forms.Label();
+            this.playerNameTextBox = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.startButton = new System.Windows.Forms.Button();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // score
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreLabel.Location = new System.Drawing.Point(16, 11);
+            this.scoreLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(81, 25);
+            this.scoreLabel.TabIndex = 0;
+            this.scoreLabel.Text = "Очки: 0";
+            // penalty
+            this.penaltyLabel.AutoSize = true;
+            this.penaltyLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.penaltyLabel.Location = new System.Drawing.Point(16, 48);
+            this.penaltyLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.penaltyLabel.Name = "penaltyLabel";
+            this.penaltyLabel.Size = new System.Drawing.Size(102, 25);
+            this.penaltyLabel.TabIndex = 1;
+            this.penaltyLabel.Text = "Штраф: 0";
+            // playerNameTextBox
+            this.playerNameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playerNameTextBox.Location = new System.Drawing.Point(21, 113);
+            this.playerNameTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.playerNameTextBox.Name = "playerNameTextBox";
+            this.playerNameTextBox.Size = new System.Drawing.Size(140, 30);
+            this.playerNameTextBox.TabIndex = 2;
+            this.playerNameTextBox.TextChanged += new System.EventHandler(this.playerNameTextBox_TextChanged);
+            // timer1
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // startButton
+            this.startButton.Location = new System.Drawing.Point(21, 150);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(129, 30);
+            this.startButton.TabIndex = 3;
+            this.startButton.Text = "Старт";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // nameLabel
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameLabel.Location = new System.Drawing.Point(16, 84);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(130, 25);
+            this.nameLabel.TabIndex = 4;
+            this.nameLabel.Text = "Введіть ім\'я:";
+            // Form1
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.playerNameTextBox);
+            this.Controls.Add(this.penaltyLabel);
+            this.Controls.Add(this.scoreLabel);
+            this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Попади в кульку";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private bool isGameStarted = false;
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.Label penaltyLabel;
+        private System.Windows.Forms.TextBox playerNameTextBox;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label nameLabel;
+    }
+}
---
+++ Toy/Form1.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Toy
+{
+    public partial class Form1 : Form
+    {
+        private const int MaxPenalty = 100;
+        private const int CircleRadius = 30;
+        private const int CircleLifetime = 2000; 
+
+        private int score;
+        private int penalty;
+        private Random random;
+        private List<Circle> circles;
+        private DateTime startTime;
+        private List<PlayerScore> playerScores;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            score = 0;
+            penalty = 0;
+            random = new Random();
+            circles = new List<Circle>();
+            startTime = DateTime.Now;
+            playerScores = new List<PlayerScore>();
+            UpdateScoreLabel();
+            timer1.Start();
+        }
+
+        private void UpdateScoreLabel()
+        {
+            scoreLabel.Text = $"Очки: {score}";
+            penaltyLabel.Text = $"Штраф: {penalty}";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (isGameStarted)
+            {
+                GenerateCircle();
+
+                if (penalty >= MaxPenalty)
+                {
+                    GameOver();
+                    return;
+                }
+
+                if ((DateTime.Now - startTime).TotalMilliseconds >= CircleLifetime)
+                {
+                    GenerateCircle();
+                    startTime = DateTime.Now;
+                }
+
+                for (int i = circles.Count - 1; i >= 0; i--)
+                {
+                    Circle circle = circles[i];
+                    circle.Lifetime -= timer1.Interval;
+
+                    if (circle.Lifetime <= 0)
+                    {
+                        circles.RemoveAt(i);
+                        penalty += GetCirclePenalty(circle.Color);
+                        UpdateScoreLabel();
+                    }
+                }
+                Invalidate();
+            }
+        }
+
+        private void GenerateCircle()
+        {
+            Random random = new Random();
+            int x = random.Next(CircleRadius, Width - CircleRadius);
+            int y = random.Next(CircleRadius, Height - CircleRadius);
+
+            Color color = GetRandomColor();
+            int lifetime = 2000;
+
+            Circle circle = new Circle(x, y, color, lifetime);
+            circles.Add(circle);
+            Invalidate();
+        }
+
+        private Color GetRandomColor()
+        {
+            Random random = new Random();
+            int colorIndex = random.Next(3);
+
+            switch (colorIndex)
+            {
+                case 0:
+                    return Color.Red;
+                case 1:
+                    return Color.Yellow;
+                case 2:
+                    return Color.Green;
+                default:
+                    return Color.Red;
+            }
+        }
+
+
+        private int GetCirclePenalty(Color color)
+        {
+            if (color == Color.Red)
+                return 10;
+            if (color == Color.Green)
+                return 20;
+            if (color == Color.Yellow)
+                return 15;
+
+            return 0;
+        }
+
+        private void StartGame()
+        {
+            playerNameTextBox.Enabled = false;
+            score = 0;
+            penalty = 0;
+            circles.Clear();
+            timer1.Start();
+            isGameStarted = true;
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            StartGame();
+        }
+
+        private void GameOver()
+        {
+            timer1.Stop();
+            SavePlayerScore();
+            ShowPlayerScores();
+            MessageBox.Show("Гра закінчена! Ваш рахунок: " + score);
+            Close();
+        }
+
+        private void SavePlayerScore()
+        {
+            string playerName = playerNameTextBox.Text;
+            PlayerScore playerScore = new PlayerScore(playerName, score, DateTime.Now);
+            playerScores.Add(playerScore);
+            playerScores = playerScores.OrderByDescending(ps => ps.Score).ToList();
+        }
+
+        private void ShowPlayerScores()
+        {
+            string leaderboard = "Рейтинг учасників:\n\n";
+            int count = Math.Min(playerScores.Count, 10);
+            for (int i = 0; i < count; i++)
+            {
+                PlayerScore playerScore = playerScores[i];
+                leaderboard += $"{i + 1}. {playerScore.Name} - {playerScore.Score} очків ({playerScore.DateTime})\n";
+            }
+            MessageBox.Show(leaderboard);
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            for (int i = circles.Count - 1; i >= 0; i--)
+            {
+                Circle circle = circles[i];
+                if (IsInsideCircle(circle, e.Location))
+                {
+                    circles.RemoveAt(i);
+                    int circleScore = GetCircleScore(circle.Color);
+                    score += circleScore;
+                    penalty += 0;
+                    UpdateScoreLabel();
+                    break;
+                }
+            }
+        }
+
+        private bool IsInsideCircle(Circle circle, Point location)
+        {
+            int distance = (int)Math.Sqrt(
+                Math.Pow(location.X - circle.X, 2) + Math.Pow(location.Y - circle.Y, 2)
+            );
+            return distance <= CircleRadius;
+        }
+
+        private int GetCircleScore(Color color)
+        {
+            if (color == Color.Red)
+                return 10;
+            if (color == Color.Green)
+                return 20;
+            if (color == Color.Yellow)
+                return 15;
+            return 0;
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            foreach (Circle circle in circles)
+            {
+                using (Brush brush = new SolidBrush(circle.Color))
+                {
+                    e.Graphics.FillEllipse(brush,
+                        circle.X - CircleRadius, circle.Y - CircleRadius,
+                        CircleRadius * 2, CircleRadius * 2);
+                }
+            }
+        }
+
+        private void playerNameTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class Circle
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Color Color { get; set; }
+        public int Lifetime { get; set; }
+
+        public Circle(int x, int y, Color color, int lifetime)
+        {
+            X = x;
+            Y = y;
+            Color = color;
+            Lifetime = lifetime;
+        }
+    }
+
+    public class PlayerScore
+    {
+        public string Name { get; set; }
+        public int Score { get; set; }
+        public DateTime DateTime { get; set; }
+
+        public PlayerScore(string name, int score, DateTime dateTime)
+        {
+            Name = name;
+            Score = score;
+            DateTime = dateTime;
+        }
+    }
+}
